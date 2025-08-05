7ad701271f1c9595da412fa660fd730d82065ef1
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-08-05 10:33:04 -0700

    neovim: Switch to Based Pyright for static type checking

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -24,7 +24,7 @@ require('mason-lspconfig').setup({
         'lua_ls', -- Lua LSP
         'rust_analyzer', -- Rust LSP
         'clangd', -- C/C++ LSP
-        'pylsp', -- Python LSP
+        'basedpyright', -- Python LSP
         'ruff',  -- Python formatter & linter
 
     },
@@ -60,14 +60,37 @@ lspconfig.clangd.setup{ -- C/C++ settings
 
 }
 
-lspconfig.pylsp.setup{ -- Python settings
-
-}
-
-vim.lsp.config('ruff', { -- Ruff settings (linter & formatter for Python)
-    init_options = {
-        settings = {
-            -- Ruff language server settings
+vim.lsp.config('basedpyright', { -- Python settings
+    settings = {
+        basedpyright = {
+            -- Use Ruff's import organizer
+            disabledOrganizeImports = true,
+        },
+        python = {
+            analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = {'*'},
+            }
         }
     }
+})
+
+vim.lsp.config('ruff', { -- Ruff settings (linter & formatter for Python)
+})
+
+-- Have Ruff defer to Based Pyright for certain capabilities
+-- See: https://docs.astral.sh/ruff/editors/setup/#neovim
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Based Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
 })
